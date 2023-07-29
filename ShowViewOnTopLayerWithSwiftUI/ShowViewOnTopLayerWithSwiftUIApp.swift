1ac7a3d3473a319bcfac6e1cc645f76a40b655c7
Gaurav Tak <gaurav.tak@roro.io>
2023-07-29 17:27:34 +0530

    Initial Commit

---
+++ ShowViewOnTopLayerWithSwiftUI/ShowViewOnTopLayerWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowViewOnTopLayerWithSwiftUIApp.swift
+//  ShowViewOnTopLayerWithSwiftUI
+//
+//  Created by Roro Solutions LLP on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ShowViewOnTopLayerWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
