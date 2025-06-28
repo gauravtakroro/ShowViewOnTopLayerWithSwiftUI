7407428fa0a19956124dd35af0b533d6beedb817
Gaurav Tak <gaurav.tak@roro.io>
2025-06-28 20:06:08 +0530

    hitTest Behaviour corrected

--- ShowViewOnTopLayerWithSwiftUI/PassThroughWindow.swift
+++ ShowViewOnTopLayerWithSwiftUI/PassThroughWindow.swift
@@ -7,11 +7,75 @@
 
 import UIKit
 
+
 class PassThroughWindow: UIWindow {
+    private static func _hitTest(
+        _ point: CGPoint,
+        with event: UIEvent?,
+        view: UIView,
+        depth: Int = 0
+    ) -> Optional<(view: UIView, depth: Int)> {
+        var deepest: Optional<(view: UIView, depth: Int)> = .none
+        
+        /// views are ordered back-to-front
+        for subview in view.subviews.reversed() {
+            let converted = view.convert(point, to: subview)
+            
+            guard subview.isUserInteractionEnabled,
+                  !subview.isHidden,
+                  subview.alpha > 0,
+                  subview.point(inside: converted, with: event)
+            else {
+                continue
+            }
+            
+            let result = if let hit = Self._hitTest(
+                converted,
+                with: event,
+                view: subview,
+                depth: depth + 1
+            ) {
+                hit
+            } else  {
+                (view: subview, depth: depth)
+            }
+            
+            if case .none = deepest {
+                deepest = result
+            } else if let current = deepest, result.depth > current.depth {
+                deepest = result
+            }
+        }
+        
+        return deepest
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        if #available(iOS 18, *) {
+            guard let view = rootViewController?.view else {
+                return false
+            }
+            
+            let hit = Self._hitTest(
+                point,
+                with: event,
+                view: subviews.count > 1 ? self : view
+            )
+            
+            return hit != nil
+        } else {
+            return super.point(inside: point, with: event)
+        }
+    }
+    
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        // Get view from superclass.
-        guard let hitView = super.hitTest(point, with: event) else { return nil }
-        // If the returned view is the `UIHostingController`'s view, ignore.
-        return rootViewController?.view == hitView ? nil : hitView
+        if #available(iOS 18, *) {
+            return super.hitTest(point, with: event)
+        } else {
+            guard let hit = super.hitTest(point, with: event) else {
+                return .none
+            }
+            return rootViewController?.view == hit ? .none : hit
+        }
     }
 }
